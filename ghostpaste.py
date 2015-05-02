e41ed7007f93bfe32bc12687b33b3099439b49c4
ken joe <kenjoe41.nafuti@gmail.com>
2015-05-02 13:34:15 +0300

    Finished a working version

--- ghostpaste.py
+++ ghostpaste.py
@@ -1,63 +1,76 @@
-#get expire time and verify commandline format given
-#get language ID from /languages.json
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-import getpass, os, json
-import requests
+#get expire time and verify commandline format given
+
 from __future__ import print_function
-import linguist
+import getpass, os, json, argparse, sys
+import requests
+from linguist.libs.file_blob import FileBlob
+requests.packages.urllib3.disable_warnings()
 
 
 def is_valid_file(parser,arg):
 	"""verify the validity of the given file. Never trust the End-User"""
 	if not os.path.exists(arg):
        		parser.error("File %s not found"%arg)
-    	else:
-        	return arg
+	else:
+	       	return arg
 
 def read_file(infilename):
-    try:
-        infile = open(infilename, 'r')
-        content = infile.read()
-	#content size here
-        infile.close()
-        return content
-    except IOError as err:
-	print("Error reading file: %s"%str(err), file=sys.stderr)
+	try:
+		with open(infilename, 'rb') as f:
+			content = f.read()
+			#content size here
+			return content
+	except IOError as err:
+		print("Error reading file: %s"%str(err), file=sys.stderr)
+		sys.exit()
 
 def download_file(url):
 	filename = url.split('/')[-1]
 
-	print "Downloading %s file." % filename
-	r = requests.get(url, stream=True)
+	print ("Downloading \"%s\" file." % filename)
+	r = requests.get(url, stream=True, verify=False)
 	with open(filename, 'wb') as f:
 		for chunk in r.iter_content(chunk_size=1024): 
 			if chunk:
 				f.write(chunk)
 				f.flush()
+	print('File downloading...Done.')
 	return filename
 
 def getID(code_file):
 	"""Get the language ID of the input file language"""
-	json_file = './languages.json'
-	lang = (detect_lang(code_file) if detect_lang(code_file) is not None)
+	json_file = 'languages.json'
 	
 	if os.path.exists(json_file):
 		pass
 	else:
 		json_file = download_file('https://ghostbin.com/languages.json')
 
-	data = json.load(read_file(json_file))#don't think i need this though
-	ID = data.read().split(lang + "\",\"id\":\"")[1]#doesn't take care of alt_id yet, squeeze HTH/Spacecow
-	return ID
+	lang = detect_lang(code_file)
+
+	json_data = json.load(file(json_file))#don't think i need this though
+	ID = ''
+	for  i in range(len(json_data)):
+		temp = len(json_data[i]['languages'])
+		for j in range(temp):	
+			if json_data[i]['languages'][j]['name'].lower() == lang.lower():
+				ID = json_data[i]['languages'][j]['id']
+				print('Gotten language ID from \'languages.json\': {0}'.format(ID))
+				return ID
 def detect_lang(path):
 	"""Detect the language used in the given file."""
 	blob = FileBlob(path, os.getcwd())
 	if blob.is_text:
+		print('Programming language of the file detected: {0}'.format(blob.language.name))
 		return blob.language.name
 	else:#images, binary and what-have-you won't be pasted
-		return None
+		print('File not a text file. Exiting...')
+		sys.exit()
 
-def main(argc, argv):
+def main():
 	parser = argparse.ArgumentParser(description='Upload code/text files to Ghostbin.')
 	parser.add_argument("-f", "--file", dest="filename", required=True,
     		help="Input code/text file to upload",
@@ -69,26 +82,36 @@ def main(argc, argv):
 	
 	args = parser.parse_args()
 
-	headers = {'content-type': 'application/x-www-form-urlencoded', 'User-Agent': agent}	
+	agent = 'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)'
+	headers = {'content-type': 'application/x-www-form-urlencoded', 'User-Agent': agent}
+	params = {}
 
 	if os.stat(args.filename).st_size > 1*1024*1024: #1MB
-		return print "Sorry, file too large. Can't continue"
+		return print ("Sorry, file too large. Can't continue")
 
 	if args.passwd:
 		passwd = getpass.getpass('Password to encrypt code: ')
-		headers['passwd'] = passwd
+		param['passwd'] = passwd
 	if args.expire:
-		headers['expire'] = args.expire
-	headers['lang'] = getID(args.filename)
+		params['expire'] = args.expire
+	ID = getID(args.filename)
+	if ID is not '':
+		params['lang'] = ID
 
 	text = read_file(args.filename)
+	params['text'] = text
 
 	host = 'ghostbin.com'
 	post_url = '/paste/new'
 	url = 'https://'+host+post_url#Use https to allow encryption and authentication. API restriction
-	agent = 'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)'
 
-	r = requests.post(url, data=text, headers=headers)
+	print('Uploading file: \'{0}\''.format(args.filename))
+	r = requests.post(url, params=params, headers=headers, verify=False)
+
 	if r.status_code == requests.codes.ok:
 	#Everything went fine, print paste URL and Session cookie
-
+		print ('File Uploaded to URL: {0}'.format(r.url))
+	else:
+		print('Something went wrong: {0}. Try again'.format(r.status_code))
+if __name__ == "__main__":
+	main()
